f36055ab84552825fdaba0e8a103dd0173321c58
jovanhan2 <jovanhan2@gmail.com>
2018-11-22 12:42:58 +0000

    Updated bash script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 usage_Dir="FSharp.Analyzers.Sample.Usage/packages/analyzers/Fsharp.Analyzers.Sample/"
+packageName="FSharp.Analyzers.Sample.1.0.1.nupkg"
 cd FSharp.Analyzers.Sample
 dotnet pack --output nupkgs
-cp -r nupkgs/FSharp.Analyzers.Sample.1.0.1.nupkg ../${usage_Dir}
+echo "Compiled nupkg"
+cp -fr nupkgs/${packageName} ../${usage_Dir}
+echo "Copied nupkg to usage directory"
+cd ..
+cd FSharp.Analyzers.Sample.Usage/packages/analyzers/FSharp.Analyzers.Sample/
+unzip -o ${packageName}
+echo "Successfully unzipped and overwritten new files"
