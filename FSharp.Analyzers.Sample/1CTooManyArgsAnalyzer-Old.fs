168dc4190b0a37b77a103c68cb716476058faf25
jovanhan2 <jovanhan2@gmail.com>
2019-05-11 23:07:02 +0100

    1C - use SynEXPR to match

---
+++ FSharp.Analyzers.Sample/1CTooManyArgsAnalyzer-Old.fs
@@ -0,0 +1,224 @@
+module TooManyArgs1C
+open System
+open FSharp.Analyzers.SDK
+open Microsoft.FSharp.Compiler.SourceCodeServices
+open Microsoft.FSharp.Compiler.Range
+open System.IO
+open System.Text.RegularExpressions
+open System.Text.RegularExpressions
+let (|FirstRegexGroup|_|) pattern input =
+   let m = Regex.Match(input,pattern) 
+   if (m.Success) then Some m.Groups else None  
+
+
+
+
+let rec visitExpr memberCallHandler (e:FSharpExpr) =
+    printfn "Type of EXPR %A StringForm %s" e.Type (e.ToString())
+    match e with
+    | BasicPatterns.AddressOf(lvalueExpr) ->
+        visitExpr memberCallHandler lvalueExpr
+    | BasicPatterns.AddressSet(lvalueExpr, rvalueExpr) ->
+        visitExpr memberCallHandler lvalueExpr; visitExpr memberCallHandler rvalueExpr
+    | BasicPatterns.Application(funcExpr, typeArgs, argExprs) ->
+        visitExpr memberCallHandler funcExpr; visitExprs memberCallHandler argExprs
+    | BasicPatterns.Call(objExprOpt, memberOrFunc, typeArgs1, typeArgs2, argExprs) ->
+        // foundType e.Range typeArgs2 state
+        // let m = memberOrFunc
+        // printfn "INSIDE CALL with typeArgs1 %A typeArgs2 %A argExprs %A"  typeArgs1 typeArgs2 argExprs
+        // printfn "DisplayName %A" m.DisplayName
+        // printfn "FullType %A" m.FullType
+        // printfn "Is function? %A" m.IsTypeFunction
+        // printfn "Logical Name %A" m.LogicalName
+        // printfn "Parameters %A" m.GenericParameters
+        memberCallHandler e.Range memberOrFunc
+        visitObjArg memberCallHandler objExprOpt; visitExprs memberCallHandler argExprs
+    | BasicPatterns.Coerce(targetType, inpExpr) ->
+        visitExpr memberCallHandler inpExpr
+    | BasicPatterns.FastIntegerForLoop(startExpr, limitExpr, consumeExpr, isUp) ->
+        visitExpr memberCallHandler startExpr; visitExpr memberCallHandler limitExpr; visitExpr memberCallHandler consumeExpr
+    | BasicPatterns.ILAsm(asmCode, typeArgs, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.ILFieldGet (objExprOpt, fieldType, fieldName) ->
+        visitObjArg memberCallHandler objExprOpt
+    | BasicPatterns.ILFieldSet (objExprOpt, fieldType, fieldName, valueExpr) ->
+        visitObjArg memberCallHandler objExprOpt
+    | BasicPatterns.IfThenElse (guardExpr, thenExpr, elseExpr) ->
+        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler thenExpr; visitExpr memberCallHandler elseExpr
+    | BasicPatterns.Lambda(lambdaVar, bodyExpr) ->
+        visitExpr memberCallHandler bodyExpr
+    | BasicPatterns.Let((bindingVar, bindingExpr), bodyExpr) ->
+        let m = bindingVar
+        printfn "INSIDE Let with bindingExpr %A and bodyExpr %A"  bindingExpr bodyExpr
+        printfn "DisplayName %A" m.DisplayName
+        printfn "FullType %A" m.FullType
+        printfn "Is function? %A" m.IsTypeFunction
+        printfn "Logical Name %A" m.LogicalName
+        printfn "Parameters %A" m.GenericParameters
+   
+
+        visitExpr memberCallHandler bindingExpr; visitExpr memberCallHandler bodyExpr
+    | BasicPatterns.LetRec(recursiveBindings, bodyExpr) ->
+        List.iter (snd >> visitExpr memberCallHandler) recursiveBindings; visitExpr memberCallHandler bodyExpr
+    | BasicPatterns.NewArray(arrayType, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.NewDelegate(delegateType, delegateBodyExpr) ->
+        visitExpr memberCallHandler delegateBodyExpr
+    | BasicPatterns.NewObject(objType, typeArgs, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.NewRecord(recordType, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.NewTuple(tupleType, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.NewUnionCase(unionType, unionCase, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.Quote(quotedExpr) ->
+        visitExpr memberCallHandler quotedExpr
+    | BasicPatterns.FSharpFieldGet(objExprOpt, recordOrClassType, fieldInfo) ->
+        visitObjArg memberCallHandler objExprOpt
+    | BasicPatterns.FSharpFieldSet(objExprOpt, recordOrClassType, fieldInfo, argExpr) ->
+        visitObjArg memberCallHandler objExprOpt; visitExpr memberCallHandler argExpr
+    | BasicPatterns.Sequential(firstExpr, secondExpr) ->
+        visitExpr memberCallHandler firstExpr; visitExpr memberCallHandler secondExpr
+    | BasicPatterns.TryFinally(bodyExpr, finalizeExpr) ->
+        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler finalizeExpr
+    | BasicPatterns.TryWith(bodyExpr, _, _, catchVar, catchExpr) ->
+        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler catchExpr
+    | BasicPatterns.TupleGet(tupleType, tupleElemIndex, tupleExpr) ->
+        visitExpr memberCallHandler tupleExpr
+    | BasicPatterns.DecisionTree(decisionExpr, decisionTargets) ->
+        visitExpr memberCallHandler decisionExpr; List.iter (snd >> visitExpr memberCallHandler) decisionTargets
+    | BasicPatterns.DecisionTreeSuccess (decisionTargetIdx, decisionTargetExprs) ->
+        visitExprs memberCallHandler decisionTargetExprs
+    | BasicPatterns.TypeLambda(genericParam, bodyExpr) ->
+        visitExpr memberCallHandler bodyExpr
+    | BasicPatterns.TypeTest(ty, inpExpr) ->
+        visitExpr memberCallHandler inpExpr
+    | BasicPatterns.UnionCaseSet(unionExpr, unionType, unionCase, unionCaseField, valueExpr) ->
+        visitExpr memberCallHandler unionExpr; visitExpr memberCallHandler valueExpr
+    | BasicPatterns.UnionCaseGet(unionExpr, unionType, unionCase, unionCaseField) ->
+        visitExpr memberCallHandler unionExpr
+    | BasicPatterns.UnionCaseTest(unionExpr, unionType, unionCase) ->
+        visitExpr memberCallHandler unionExpr
+    | BasicPatterns.UnionCaseTag(unionExpr, unionType) ->
+        visitExpr memberCallHandler unionExpr
+    | BasicPatterns.ObjectExpr(objType, baseCallExpr, overrides, interfaceImplementations) ->
+        visitExpr memberCallHandler baseCallExpr
+        List.iter (visitObjMember memberCallHandler) overrides
+        List.iter (snd >> List.iter (visitObjMember memberCallHandler)) interfaceImplementations
+    | BasicPatterns.TraitCall(sourceTypes, traitName, typeArgs, typeInstantiation, argTypes, argExprs) ->
+        visitExprs memberCallHandler argExprs
+    | BasicPatterns.ValueSet(valToSet, valueExpr) ->
+        visitExpr memberCallHandler valueExpr
+    | BasicPatterns.WhileLoop(guardExpr, bodyExpr) ->
+        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler bodyExpr
+    | BasicPatterns.BaseValue baseType -> ()
+    | BasicPatterns.DefaultValue defaultType -> ()
+    | BasicPatterns.ThisValue thisType -> ()
+    | BasicPatterns.Const(constValueObj, constType) -> ()
+    | BasicPatterns.Value(valueToGet) -> ()
+    | _ -> ()
+
+and visitExprs f exprs =
+    List.iter (visitExpr f) exprs
+
+and visitObjArg f objOpt =
+    Option.iter (visitExpr f) objOpt
+
+and visitObjMember f memb =
+    visitExpr f memb.Body
+
+let rec visitDeclaration f d =
+    match d with
+    | FSharpImplementationFileDeclaration.Entity (e, subDecls) ->
+        for subDecl in subDecls do
+            visitDeclaration f subDecl
+    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) ->
+        visitExpr f e
+    | FSharpImplementationFileDeclaration.InitAction(e) ->
+        visitExpr f e
+
+let checker = FSharpChecker.Create(keepAssemblyContents=true)
+let parseAndCheckSingleFile (input) = 
+    let file = Path.ChangeExtension(System.IO.Path.GetTempFileName(), "fsx")  
+    File.WriteAllText(file, input)
+    // Get context representing a stand-alone (script) file
+    let projOptions, _errors = 
+        checker.GetProjectOptionsFromScript(file, input)
+        |> Async.RunSynchronously
+
+    let fprojOptions = projOptions
+
+    checker.ParseAndCheckProject (fprojOptions)
+    |> Async.RunSynchronously
+let testRegex str = 
+    match str with
+     // | FirstRegexGroup "error (FS\d\d\d\d): The type ''a -> 'b' does not match the type '(.*)'" host -> 
+    | FirstRegexGroup "The type '(.*) -> (.*)' does not match the type '(.*)'" host -> 
+        //    printfn "Found %s Error with type %s" host.[1].Value host.[2].Value
+           "FS0001"
+        //    (host.[1].Value,host.[2].Value, host.[3].Value)
+    | _ -> ""
+        // ("","", "")
+        //    printfn "The value '%s' is something else" str
+        //    The type 'int -> obj' does not match the type 'int'
+
+[<Analyzer>]
+let TooManyArgs1C : Analyzer =
+    printfn "1CTooManyArgs Analyzer"
+    fun ctx ->
+        printfn "CTX string %A" ctx
+        let state = ResizeArray<range>()
+        let string = ctx.Content |> String.concat "\n"
+        let checkProjectResults = parseAndCheckSingleFile(string)
+        let handler (range: range) (m: FSharpMemberOrFunctionOrValue) =()
+        ctx.TypedTree.Declarations |> List.iter (visitDeclaration handler)
+        // printfn "Errors: %A" checkProjectResults.Errors
+        // for i = 1 to checkProjectResults.Errors.Length do
+        //     printfn "Error %d %s" i checkProjectResults.Errors.[i].Message
+
+        //     printfn "Subcategory: %s" checkProjectResults.Errors.[i].Subcategory
+
+        //     printfn "Error number: %d" checkProjectResults.Errors.[i].ErrorNumber
+        //     printfn "\n"
+        //     // printfn "Error info %A" x
+        //     // printfn "Error Number %i" x.ErrorNumber
+        //     // printfn "Start column %i" x.StartColumn
+        //     // printfn "end column %i" x.EndColumn
+        //     // printfn "StartLine alternate %i" x.StartLineAlternate
+        //     // printfn "EndLine alternate %i" x.EndLineAlternate
+        //     // printfn "Error message %s" x.Message
+        //     let t = testRegex checkProjectResults.Errors.[i].Message
+
+        //     // if x.Subcategory = "typecheck" && t = "FS0001" then
+        //     // printfn "Inside FS0001"
+        //     let handler (range: range) (m: FSharpMemberOrFunctionOrValue) =
+        //         ()
+        //         // printfn "DisplayName %A" m.DisplayName
+        //         // printfn "FullType %A" m.FullType
+        //         // printfn "Is function? %A" m.IsTypeFunction
+        //         // printfn "Logical Name %A" m.LogicalName
+        //         // printfn "Parameters %A" m.GenericParameters
+        //         // printfn "m %A" m
+        //         // printfn "Parameters %A" m.GenericParameters
+        //         // printfn "FullType %A" m.FullType
+        //         // printfn "Curried Parameter Groups %A" m.CurriedParameterGroups
+        //         // printfn "range %A" range
+        //         // printfn "typedTreeDeclarations %A" ctx.TypedTree.Declarations 
+        //         // if m.ToString() = "val op_Range" then
+        //         //     state.Add range
+        //         //     printfn"Added op_range %A" range
+        //     ctx.TypedTree.Declarations |> List.iter (visitDeclaration handler)
+           
+
+        state
+        |> Seq.map (fun r ->
+            { Type = "MismatchedTypes Analyzer"
+              Message = "Using List Operations usually requires float instead of int"
+              Code = "A"
+              Severity = Warning
+              Range = r
+              Fixes = []}
+
+        )
+        |> Seq.toList
--- FSharp.Analyzers.Sample/1CTooManyArgsAnalyzer.fs
+++ FSharp.Analyzers.Sample/1CTooManyArgsAnalyzer.fs
@@ -1,221 +1,72 @@
-module TooManyArgs1C
-open System
+module ToomanyArgs
+
 open FSharp.Analyzers.SDK
-open Microsoft.FSharp.Compiler.SourceCodeServices
+// open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler.Range
-open System.IO
-open System.Text.RegularExpressions
-open System.Text.RegularExpressions
-let (|FirstRegexGroup|_|) pattern input =
-   let m = Regex.Match(input,pattern) 
-   if (m.Success) then Some m.Groups else None  
+open Microsoft.FSharp.Compiler.Ast
+
+
+let rec visitExpression handler= 
+    function 
+    | SynExpr.App(exprAtomicFlag, isInfix, funcExpr, argExpr, m) -> 
+        visitExpression handler funcExpr
+        visitExpression handler argExpr
+    | SynExpr.LetOrUse(isRecurisve,isUse,bindings,body,range) ->
+        printfn "Let isUse %A" isUse
+        printfn "Bindings %A" bindings
+        printfn "Body %A" body
+        printfn "range %A" range
+    | _ -> printfn "unmatched!"
+        
+
+
+let visitDeclarations handler decls = 
+    for declaration in decls do
+        match declaration with
+        | SynModuleDecl.Let(isRec, bindings, range) -> 
+            for binding in bindings do
+                let (Binding(access, kind, inlin, mutabl, attrs, xmlDoc, data, 
+                             pat, retInfo, body, m, sp)) = binding
+                printfn "in visitExpression Let"
+                visitExpression handler body
+        | _ -> ()
+
+let visitModulesAndNamespaces handler modulesOrNss = 
+    let (SynModuleOrNamespace(lid, isRec, isMod, decls, xml, attrs, _, m)) = 
+        modulesOrNss
+    printfn "Namespace or module: %A" lid
+    visitDeclarations handler decls
 
 
 
-
-let rec visitExpr memberCallHandler (e:FSharpExpr) =
-    printfn "Type of EXPR %A StringForm %s" e.Type (e.ToString())
-    match e with
-    | BasicPatterns.AddressOf(lvalueExpr) ->
-        visitExpr memberCallHandler lvalueExpr
-    | BasicPatterns.AddressSet(lvalueExpr, rvalueExpr) ->
-        visitExpr memberCallHandler lvalueExpr; visitExpr memberCallHandler rvalueExpr
-    | BasicPatterns.Application(funcExpr, typeArgs, argExprs) ->
-        visitExpr memberCallHandler funcExpr; visitExprs memberCallHandler argExprs
-    | BasicPatterns.Call(objExprOpt, memberOrFunc, typeArgs1, typeArgs2, argExprs) ->
-        // foundType e.Range typeArgs2 state
-        // let m = memberOrFunc
-        // printfn "INSIDE CALL with typeArgs1 %A typeArgs2 %A argExprs %A"  typeArgs1 typeArgs2 argExprs
-        // printfn "DisplayName %A" m.DisplayName
-        // printfn "FullType %A" m.FullType
-        // printfn "Is function? %A" m.IsTypeFunction
-        // printfn "Logical Name %A" m.LogicalName
-        // printfn "Parameters %A" m.GenericParameters
-        memberCallHandler e.Range memberOrFunc
-        visitObjArg memberCallHandler objExprOpt; visitExprs memberCallHandler argExprs
-    | BasicPatterns.Coerce(targetType, inpExpr) ->
-        visitExpr memberCallHandler inpExpr
-    | BasicPatterns.FastIntegerForLoop(startExpr, limitExpr, consumeExpr, isUp) ->
-        visitExpr memberCallHandler startExpr; visitExpr memberCallHandler limitExpr; visitExpr memberCallHandler consumeExpr
-    | BasicPatterns.ILAsm(asmCode, typeArgs, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.ILFieldGet (objExprOpt, fieldType, fieldName) ->
-        visitObjArg memberCallHandler objExprOpt
-    | BasicPatterns.ILFieldSet (objExprOpt, fieldType, fieldName, valueExpr) ->
-        visitObjArg memberCallHandler objExprOpt
-    | BasicPatterns.IfThenElse (guardExpr, thenExpr, elseExpr) ->
-        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler thenExpr; visitExpr memberCallHandler elseExpr
-    | BasicPatterns.Lambda(lambdaVar, bodyExpr) ->
-        visitExpr memberCallHandler bodyExpr
-    | BasicPatterns.Let((bindingVar, bindingExpr), bodyExpr) ->
-        let m = bindingVar
-        printfn "INSIDE Let with bindingExpr %A and bodyExpr %A"  bindingExpr bodyExpr
-        printfn "DisplayName %A" m.DisplayName
-        printfn "FullType %A" m.FullType
-        printfn "Is function? %A" m.IsTypeFunction
-        printfn "Logical Name %A" m.LogicalName
-        printfn "Parameters %A" m.GenericParameters
-   
-
-        visitExpr memberCallHandler bindingExpr; visitExpr memberCallHandler bodyExpr
-    | BasicPatterns.LetRec(recursiveBindings, bodyExpr) ->
-        List.iter (snd >> visitExpr memberCallHandler) recursiveBindings; visitExpr memberCallHandler bodyExpr
-    | BasicPatterns.NewArray(arrayType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.NewDelegate(delegateType, delegateBodyExpr) ->
-        visitExpr memberCallHandler delegateBodyExpr
-    | BasicPatterns.NewObject(objType, typeArgs, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.NewRecord(recordType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.NewTuple(tupleType, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.NewUnionCase(unionType, unionCase, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.Quote(quotedExpr) ->
-        visitExpr memberCallHandler quotedExpr
-    | BasicPatterns.FSharpFieldGet(objExprOpt, recordOrClassType, fieldInfo) ->
-        visitObjArg memberCallHandler objExprOpt
-    | BasicPatterns.FSharpFieldSet(objExprOpt, recordOrClassType, fieldInfo, argExpr) ->
-        visitObjArg memberCallHandler objExprOpt; visitExpr memberCallHandler argExpr
-    | BasicPatterns.Sequential(firstExpr, secondExpr) ->
-        visitExpr memberCallHandler firstExpr; visitExpr memberCallHandler secondExpr
-    | BasicPatterns.TryFinally(bodyExpr, finalizeExpr) ->
-        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler finalizeExpr
-    | BasicPatterns.TryWith(bodyExpr, _, _, catchVar, catchExpr) ->
-        visitExpr memberCallHandler bodyExpr; visitExpr memberCallHandler catchExpr
-    | BasicPatterns.TupleGet(tupleType, tupleElemIndex, tupleExpr) ->
-        visitExpr memberCallHandler tupleExpr
-    | BasicPatterns.DecisionTree(decisionExpr, decisionTargets) ->
-        visitExpr memberCallHandler decisionExpr; List.iter (snd >> visitExpr memberCallHandler) decisionTargets
-    | BasicPatterns.DecisionTreeSuccess (decisionTargetIdx, decisionTargetExprs) ->
-        visitExprs memberCallHandler decisionTargetExprs
-    | BasicPatterns.TypeLambda(genericParam, bodyExpr) ->
-        visitExpr memberCallHandler bodyExpr
-    | BasicPatterns.TypeTest(ty, inpExpr) ->
-        visitExpr memberCallHandler inpExpr
-    | BasicPatterns.UnionCaseSet(unionExpr, unionType, unionCase, unionCaseField, valueExpr) ->
-        visitExpr memberCallHandler unionExpr; visitExpr memberCallHandler valueExpr
-    | BasicPatterns.UnionCaseGet(unionExpr, unionType, unionCase, unionCaseField) ->
-        visitExpr memberCallHandler unionExpr
-    | BasicPatterns.UnionCaseTest(unionExpr, unionType, unionCase) ->
-        visitExpr memberCallHandler unionExpr
-    | BasicPatterns.UnionCaseTag(unionExpr, unionType) ->
-        visitExpr memberCallHandler unionExpr
-    | BasicPatterns.ObjectExpr(objType, baseCallExpr, overrides, interfaceImplementations) ->
-        visitExpr memberCallHandler baseCallExpr
-        List.iter (visitObjMember memberCallHandler) overrides
-        List.iter (snd >> List.iter (visitObjMember memberCallHandler)) interfaceImplementations
-    | BasicPatterns.TraitCall(sourceTypes, traitName, typeArgs, typeInstantiation, argTypes, argExprs) ->
-        visitExprs memberCallHandler argExprs
-    | BasicPatterns.ValueSet(valToSet, valueExpr) ->
-        visitExpr memberCallHandler valueExpr
-    | BasicPatterns.WhileLoop(guardExpr, bodyExpr) ->
-        visitExpr memberCallHandler guardExpr; visitExpr memberCallHandler bodyExpr
-    | BasicPatterns.BaseValue baseType -> ()
-    | BasicPatterns.DefaultValue defaultType -> ()
-    | BasicPatterns.ThisValue thisType -> ()
-    | BasicPatterns.Const(constValueObj, constType) -> ()
-    | BasicPatterns.Value(valueToGet) -> ()
-    | _ -> ()
-
-and visitExprs f exprs =
-    List.iter (visitExpr f) exprs
-
-and visitObjArg f objOpt =
-    Option.iter (visitExpr f) objOpt
-
-and visitObjMember f memb =
-    visitExpr f memb.Body
-
-let rec visitDeclaration f d =
-    match d with
-    | FSharpImplementationFileDeclaration.Entity (e, subDecls) ->
-        for subDecl in subDecls do
-            visitDeclaration f subDecl
-    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) ->
-        visitExpr f e
-    | FSharpImplementationFileDeclaration.InitAction(e) ->
-        visitExpr f e
-
-let checker = FSharpChecker.Create(keepAssemblyContents=true)
-let parseAndCheckSingleFile (input) = 
-    let file = Path.ChangeExtension(System.IO.Path.GetTempFileName(), "fsx")  
-    File.WriteAllText(file, input)
-    // Get context representing a stand-alone (script) file
-    let projOptions, _errors = 
-        checker.GetProjectOptionsFromScript(file, input)
-        |> Async.RunSynchronously
-
-    let fprojOptions = projOptions
-
-    checker.ParseAndCheckProject (fprojOptions)
-    |> Async.RunSynchronously
-let testRegex str = 
-    match str with
-     // | FirstRegexGroup "error (FS\d\d\d\d): The type ''a -> 'b' does not match the type '(.*)'" host -> 
-    | FirstRegexGroup "The type '(.*) -> (.*)' does not match the type '(.*)'" host -> 
-        //    printfn "Found %s Error with type %s" host.[1].Value host.[2].Value
-           "FS0001"
-        //    (host.[1].Value,host.[2].Value, host.[3].Value)
-    | _ -> ""
-        // ("","", "")
-        //    printfn "The value '%s' is something else" str
-        //    The type 'int -> obj' does not match the type 'int'
 
 [<Analyzer>]
-let TooManyArgs1C : Analyzer =
-    printfn "1CTooManyArgs Analyzer"
+let paranthesis : Analyzer =
+    printfn "Inside tooMany args!"
+    
     fun ctx ->
-        printfn "CTX string %A" ctx
+        printfn "ctx %A" ctx.ParseTree
         let state = ResizeArray<range>()
-        let string = ctx.Content |> String.concat "\n"
-        let checkProjectResults = parseAndCheckSingleFile(string)
-        let handler (range: range) (m: FSharpMemberOrFunctionOrValue) =()
-        ctx.TypedTree.Declarations |> List.iter (visitDeclaration handler)
-        // printfn "Errors: %A" checkProjectResults.Errors
-        // for i = 1 to checkProjectResults.Errors.Length do
-        //     printfn "Error %d %s" i checkProjectResults.Errors.[i].Message
-
-        //     printfn "Subcategory: %s" checkProjectResults.Errors.[i].Subcategory
-
-        //     printfn "Error number: %d" checkProjectResults.Errors.[i].ErrorNumber
-        //     printfn "\n"
-        //     // printfn "Error info %A" x
-        //     // printfn "Error Number %i" x.ErrorNumber
-        //     // printfn "Start column %i" x.StartColumn
-        //     // printfn "end column %i" x.EndColumn
-        //     // printfn "StartLine alternate %i" x.StartLineAlternate
-        //     // printfn "EndLine alternate %i" x.EndLineAlternate
-        //     // printfn "Error message %s" x.Message
-        //     let t = testRegex checkProjectResults.Errors.[i].Message
-
-        //     // if x.Subcategory = "typecheck" && t = "FS0001" then
-        //     // printfn "Inside FS0001"
-        //     let handler (range: range) (m: FSharpMemberOrFunctionOrValue) =
-        //         ()
-        //         // printfn "DisplayName %A" m.DisplayName
-        //         // printfn "FullType %A" m.FullType
-        //         // printfn "Is function? %A" m.IsTypeFunction
-        //         // printfn "Logical Name %A" m.LogicalName
-        //         // printfn "Parameters %A" m.GenericParameters
-        //         // printfn "m %A" m
-        //         // printfn "Parameters %A" m.GenericParameters
-        //         // printfn "FullType %A" m.FullType
-        //         // printfn "Curried Parameter Groups %A" m.CurriedParameterGroups
-        //         // printfn "range %A" range
-        //         // printfn "typedTreeDeclarations %A" ctx.TypedTree.Declarations 
-        //         // if m.ToString() = "val op_Range" then
-        //         //     state.Add range
-        //         //     printfn"Added op_range %A" range
-        //     ctx.TypedTree.Declarations |> List.iter (visitDeclaration handler)
-           
-
+        // handler adds the range to display
+        let handler (range: range) (m: SynExpr) =
+            printfn "###################################"
+            printfn "SynExpr type %A" m
+            printfn "###################################"
+            state.Add range
+        let parseTree = ctx.ParseTree
+        match parseTree with
+        | ParsedInput.ImplFile(implFile) ->
+            // Extract declarations and walk over them
+            let (ParsedImplFileInput(fn, script, name, _, _, modules, _)) = implFile
+            modules |>  List.iter (visitModulesAndNamespaces handler)
+            // visitModulesAndNamespaces modules
+        | _ -> failwith "F# Interface file (*.fsi) not supported."
+        // parseTree.
         state
         |> Seq.map (fun r ->
-            { Type = "MismatchedTypes Analyzer"
-              Message = "Using List Operations usually requires float instead of int"
-              Code = "A"
+            { Type = "Parenthesis Analyser"
+              Message = "Possible bracket error"
+              Code = "P001"
               Severity = Warning
               Range = r
               Fixes = []}
